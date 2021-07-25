f54cff852d7805db55b59dd7601c692eabc6e994
k.sunwoo <76653202+kangsunwoo827@users.noreply.github.com>
2021-07-25 17:15:41 +0900

    Update search.js

--- js/search.js
+++ js/search.js
@@ -39,12 +39,11 @@
     // Initalize lunr with the fields it will be searching on. I've given title
     // a boost of 10 to indicate matches on this field are more important.
   
-    var idx = new lunr.Index;
-    idx.field('id');
-    idx.field('title', { boost: 10 });
-    idx.field('author');
-    idx.field('category');
-    idx.field('content');
+    var idx = lunr(function () {
+      this.field('id');
+      this.field('title', { boost: 10 });
+      this.field('category');
+    });
     
     
     for (var key in window.store) { // Add the data to lunr
